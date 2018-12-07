c50255710afb50c3b321c5b6082474512bd40094
ndoell <nick.doell@gmail.com>
2018-12-07 10:55:19 -0500

    Removed random port, now using go's net.ResolveTCPAddr to find an availible port.

--- main.go
+++ main.go
@@ -1,18 +1,41 @@
 package main
 
 import (
-	"math/rand"
+//	"math/rand"
+//	"time"
+	"net"
+	"log"
 	"fmt"
-	"time"
 )
 
-func random(min, max int) int {
-	return rand.Intn(max - min) + min
+//func randport(min, max int) int {
+//	return rand.Intn(max - min) + min
+//}
+
+//Check if a port is open by connecting.
+func GetFreePort() (int, error) {
+	addr, err := net.ResolveTCPAddr("tcp", "localhost:0")
+	if err != nil {
+		return 0, err
+	}
+
+	l, err := net.ListenTCP("tcp", addr)
+	if err != nil {
+		return 0, err
+	}
+	defer l.Close()
+	return l.Addr().(*net.TCPAddr).Port, nil
 }
-
-
 
 func main() {
-	rand.Seed(time.Now().UTC().UnixNano())
-	fmt.Println(random(5000, 9000))
+//	rand.Seed(time.Now().UTC().UnixNano())
+//	GetFreePort(randport(5000, 9000))
+
+	port, err := GetFreePort()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(port)
 }
+
